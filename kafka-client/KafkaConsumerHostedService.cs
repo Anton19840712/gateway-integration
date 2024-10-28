fa050e8fa8191667db15c1f162c0c86c35176688
Антон Гридюшко <gridjushko@pit.protei.ru>
2024-10-28 15:13:28 +0300

    Добавил кафка клиент.

---
+++ kafka-client/KafkaConsumerHostedService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using Kafka.Public.Loggers;
+using Kafka.Public;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace kafka_client;
+
+public class KafkaConsumerHostedService : IHostedService
+{
+    private readonly ILogger<KafkaConsumerHostedService> _logger;
+    private readonly ClusterClient _cluster;
+
+    public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+    {
+        _logger = logger;
+        _cluster = new ClusterClient(new Configuration
+        {
+            Seeds = "localhost:9092"
+        }, new ConsoleLogger());
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _cluster.ConsumeFromLatest("demo");
+        _cluster.MessageReceived += record =>
+        {
+            _logger.LogInformation($"Consumed: {Encoding.UTF8.GetString((record.Value as byte[])!)}");
+            //Console.WriteLine($"Consumed: {Encoding.UTF8.GetString((record.Value as byte[])!)}");
+        };
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cluster.Dispose();
+        return Task.CompletedTask;
+    }
+}
---
+++ kafka-client/Program.cs
@@ -0,0 +1,21 @@
+﻿using kafka_client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Console.Title = "consumer";
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseSerilog((context, services, configuration) =>
+        {
+            configuration
+                .WriteTo.Console()
+                .ReadFrom.Configuration(context.Configuration); // Чтение конфигурации из appsettings.json, если есть
+        })
+        .ConfigureServices((context, collection) =>
+        {
+            collection.AddHostedService<KafkaConsumerHostedService>();
+        });
